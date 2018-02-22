9376cf30b77151d45d1d99290a4f92cfa47b0cb2
keithbaldwin <keith.baldwin@dunelm.org.uk>
2018-02-22 13:57:35 -0800

    Update dockerBuild.sh

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -1,1 +1,1 @@
-docker build -t certbot --build-arg primary_domain=digatrain.com --build-arg secondary_domains=www.digatrain.com mon.digatrain.com --build-arg email=keith_baldwin@outlook.com .
+docker build -t certbot --build-arg primary_domain=***** --build-arg secondary_domains=***** --build-arg email=******* .
